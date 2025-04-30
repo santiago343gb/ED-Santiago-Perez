714f58b861a34e5faa87099fc17212ce2d552c60
santiago <santiago.perez2@isepceu.es>
2025-04-30 19:09:43 +0200

    Santiago Perez GravedadPlaneta

---
+++ Santiago_GravedadPlaneta.java
@@ -0,0 +1,49 @@
+public class Santiago_GravedadPlaneta {
+    private int id;
+    private String nombre;
+    private double GravedadPlaneta;
+
+    public Santiago_GravedadPlaneta(double GravedadPlaneta, int id, String nombre) {
+        this.GravedadPlaneta = GravedadPlaneta;
+        this.id = id;
+        this.nombre = nombre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getGravedadPlaneta() {
+        return GravedadPlaneta;
+    }
+
+    public void setGravedadPlaneta(double GravedadPlaneta) {
+        this.GravedadPlaneta = GravedadPlaneta;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Santiago_GravedadPlaneta{");
+        sb.append("id=").append(id);
+        sb.append(", nombre=").append(nombre);
+        sb.append(", GravedadPlaneta=").append(GravedadPlaneta);
+        sb.append('}');
+        return sb.toString();
+    }
+
+    
+
+}
